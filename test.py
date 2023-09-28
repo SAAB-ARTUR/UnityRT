6f97b4fd5b00cd9a9dd26e7cc96a12ce594845dc
John Daniel <daniel@bosser.com>
2023-09-28 11:14:05 +0200

    Input and output to external process works

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
+﻿import sys
+import fileinput
+
 print("Hello from Python!")
+
+for line in fileinput.input():
+	
+	print("Python got:" + line)
+
+print("Done! Okeyy byeee")
+print("Python was killed by unity 🤨")
